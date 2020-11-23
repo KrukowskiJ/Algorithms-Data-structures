3a8b05408942a001bce011610de0bf1c327b8726
Jakub Krukowski <62674442+KrukowskiJ@users.noreply.github.com>
2020-11-23 18:26:13 +0100

    implementation of fifo queue

---
+++ fifo_queue.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+template <typename T>
+
+
+class Fifo
+{
+    struct Struktura
+    {
+        T val;
+        struct Struktura* next;
+    };
+    Struktura* head = NULL;
+public:
+
+    void push(T val)
+    {
+        
+        if(head == NULL) {
+            head = new Struktura;
+            head->val = val;
+            head->next = NULL;
+        }
+        else {
+            Struktura* tmp = head;
+            while (tmp->next)
+                tmp = tmp->next;
+
+            tmp->next = new Struktura;
+            tmp->next->val = val;
+            tmp->next->next = NULL;
+        
+        
+        }
+        
+    }
+
+    void top()
+    {
+        Struktura * tmp = head;
+        while(tmp)
+        {
+            std::cout<<tmp->val;
+                tmp = tmp->next;
+        }
+    }
+    void pop()
+    {
+        Struktura* t = NULL;
+        if (head != NULL)
+        {
+            t = head->next;
+            std::cout << head->val;
+            delete head;
+            head = t;
+        }
+        else std::cout << "x";
+    }
+};
+
+int main()
+{
+    Fifo<int> s;
+    int x;
+    
+
+    char choice = '4';
+    while (choice != '3') {
+        std::cin >> choice;
+        if (choice == '0') {
+            std::cin >> x;
+            s.push(x);
+        }
+        if (choice == '1') {
+            s.pop();
+        }
+        if (choice == '2') {
+            s.top();
+        }
+
+
+    }
+    
+
+}
